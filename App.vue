6921f65b00dae6bdc6d89e5bc75547028b3794b3
jennie-xin <3542952326@qq.com>
2026-04-10 11:15:00 +0800

    init: 配置uni-app入口文件与全局样式

---
+++ App.vue
@@ -0,0 +1,17 @@
+<script>
+	export default {
+		
+	}
+</script>
+
+<style>
+	view{
+		   box-sizing: border-box;
+	}
+	::-webkit-scrollbar {
+	  display: none;
+	  width: 0;
+	  height: 0;
+	  color: transparent;
+	}
+</style>
